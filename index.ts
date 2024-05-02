0cb1088add80bc96c923e60efe57304eace0cfd3
mingderwang <admin@ming-ders-MacBook.local>
2024-05-02 16:49:10 +0800

    add remarks in code

--- index.ts
+++ index.ts
@@ -12,11 +12,17 @@ const dom = new JSDOM(html);
 const document = dom.window.document;
 const XPathResult_FIRST_ORDERED_NODE_TYPE = 9;
 
+// you can use any document function
 //console.log(dom.window.document.querySelector("p").textContent); 
+
+// or use document.evaluate() with XPATH, you can find the full XPATH for an element in devtools from a Inspection Element copy
 const result = document.evaluate(process.env.XPATH, document, null, XPathResult_FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 const obj = getJSONFromDOMElement(result);
+
+// if you want to see your current element in JSON
 //console.log(JSON.stringify(obj.childNodes, null, 2));
 
+// to print a list of text
 for (const element of obj.childNodes) {
   console.log(element.childNodes[0].childNodes[1].text);
 }
